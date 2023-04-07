7e648b53cc684e5353cd3ac930c221bbaaef2eb9
João Lima <jpalvesl101@gmail.com>
2023-04-07 17:49:12 -0300

    🌟 feat: criando service e interface para o problema

---
+++ src/types/Problema.ts
@@ -0,0 +1,11 @@
+export interface Problema {
+  id?: number;
+  descricao: string;
+  dificuldade: string;
+  fonte: string;
+  nome: string;
+  textoEntrada: string;
+  textoSaida: string;
+  criadorId: string;
+  topicos: number[];
+}
